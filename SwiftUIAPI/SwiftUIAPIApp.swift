e70aae6a64a64e9e4be2d493db7c716c4e9b807b
nochphanith <nochphanith@gmail.com>
2021-03-23 19:40:33 +0700

    Initial Commit

---
+++ SwiftUIAPI/SwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIApp.swift
+//  SwiftUIAPI
+//
+//  Created by Admin on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
